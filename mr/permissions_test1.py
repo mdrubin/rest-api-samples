c8562aa9276f2b37a2f8c91dcdf30689368efcbb
Rubin, Mark <Mark_Rubin@cable.comcast.com>
2023-01-12 22:09:55 -0500

    c

---
+++ mr/permissions_test1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 12:35:02 2019
+
+@author: MRubin001
+"""
+
+
+import sys
+
+#sys.path.append(r'C:\\Users\\mrubin001\\Desktop\\Python\\rest_api\\python')
+
+import python.user_permission_audit as upa
+upa.sign_in('tableau.comcast.com','mrubin001','sempron7&','gbr finance')
+
